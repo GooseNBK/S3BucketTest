650acaa4004f6c7f9887a62e07760d4cc2ad3008
Gustavo Borjas <49384648+GooseNBK@users.noreply.github.com>
2023-09-04 12:35:55 -0400

    Add project files.

---
+++ S3BucketTest/Controllers/BucketController.cs
@@ -0,0 +1,38 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace S3BucketTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BucketController : ControllerBase
+    {
+        private static readonly string awsSecretAccessKey = "QCwp+9c197jq0JLJQiB2nQcjie5fxnMttroV0HLA";
+        private static readonly string awsAccessKeyId = "AKIA3RXTBGWJ4YXIWFNT";
+        private static readonly string bucket = "lcatestbucket";
+        //private static readonly string folder = "";
+
+        [HttpGet]
+        public async Task<ActionResult<int>> Get()
+        {
+            var client = new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.USEast2);
+
+            ListObjectsRequest request = new ListObjectsRequest();
+            request.BucketName = bucket;
+            //request.Prefix = folder;
+            ListObjectsResponse response = await client.ListObjectsAsync(request);
+
+            int counter = 0;
+
+            foreach (S3Object o in response.S3Objects)
+            {
+                counter++;
+            }
+
+            return 10;
+        }
+    }
+}
